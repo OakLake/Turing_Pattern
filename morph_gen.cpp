1f6f62f62388215efdfc52053154b8767a79fe08
Sammy Hasan <sammy.nm.hasan@gmail.com>
2017-11-08 13:32:05 +0000

    animates

--- morph_gen.cpp
+++ morph_gen.cpp
@@ -28,27 +28,29 @@ double my_rand();
 void add_noise(Mat,int,double);
 void calc_scale(Mat,Mat,Mat,int);
 void psy_reagent(Mat,Mat,Mat,int,int,int);
-
+// void mirror_pad(Mat,Mat,int);
 ///////////////////////
 
 int main(){
   //
   srand(time(NULL));
-  int seed = 1851156740;//rand(); //
+  int seed = rand();
   theRNG().state = seed;
   cout << "Random Seed: " << seed << endl;
   //
-  Size size(600,600);
-  Mat frame(size,CV_8SC1);
+  Size size(500,500);
+  Size size_pad(600,600);
+  Mat frame(size_pad,CV_8SC1);
   randu(frame,Scalar(-127),Scalar(127));
   // circle(frame, Point(300,300), 200,Scalar(-42), -1);
   //
   //
-  int num_scales = 2;
+  int num_scales = 3;
   vector<Mat> activ_vec;
   vector<Mat> inhib_vec;
   vector<Mat> indv_scale;
   for(int v=0;v<num_scales;v++){
+
     activ_vec.push_back(Mat::zeros(size,CV_8SC1));
     inhib_vec.push_back(Mat::zeros(size,CV_8SC1));
     indv_scale.push_back(Mat::zeros(size,CV_8SC1));
@@ -58,15 +60,16 @@ int main(){
   Mat global_inhib = Mat::zeros(size,CV_8SC1);
   //
   // ONLY ODD Nums & N[*] > P[*] Square kernels/scales
-  int P[] = {13,17,23,43};
-  int N[] = {23,23,43,73};
-  int weights[] = {1,1,-1,4};
-
-  int rate = 1;
+  int P[] = {3,11,23,43};
+  int N[] = {11,79,43,73};
+  int weights[] = {20,3,1,4};
+  int thr[] = {-100,-10,42};
+  int rate[] = {-10,40,50};
+  int global_rate = 1;
   //
   int loops = 5000;
   int make_sym_FLAG = 1;
-  int Method_FLAG = 1;
+  int Method_FLAG = 2;
 
 
   namedWindow("Frame",WINDOW_AUTOSIZE);
@@ -80,16 +83,40 @@ int main(){
     cout << "\r*Iteration: " << i << flush;
 
     // threads are manually set, more than 4 is not beneficial
-    thread t1(psy_reagent,frame, activ_vec[0],inhib_vec[0], P[0], N[0],-100);
-    thread t2(psy_reagent,frame, activ_vec[1],inhib_vec[1], P[1], N[1],0);
+    // thread t1(calc_reagnet,frame, activ_vec[0],inhib_vec[0], P[0], N[0]);
+    // thread t2(calc_reagnet,frame, activ_vec[1],inhib_vec[1], P[1], N[1]);
     // thread t3(psy_reagent,frame, activ_vec[2],inhib_vec[2], P[2], N[2],-40);
     // thread t4(psy_reagent,frame, activ_vec[3],inhib_vec[3], P[3], N[3],10);
 
-    t1.join();
-    t2.join();
+    // t1.join();
+    // t2.join();
     // t3.join();
     // t4.join();
 
+    for(int s=0;s<num_scales;s++){
+      activ_vec[s] = 0;
+      inhib_vec[s] = 0;
+      for(int row=50;row<frame.rows-50;row++){
+        for(int col=50;col<frame.cols-50;col++){
+          int p = P[s];
+          int n = N[s];
+          int w_p = p/2;
+          int w_n = n/2;
+          double p_scale = mean(frame(Rect(row-w_p,col-w_p,p,p)) )[0];
+          if (p_scale >= thr[s]){
+            double n_scale = mean(frame(Rect(row-w_n,col-w_n,n,n)) )[0];
+            activ_vec[s].at<schar>(row-45,col-45) = p_scale;
+            inhib_vec[s].at<schar>(row-45,col-45) = n_scale;
+          }
+
+        }
+      }
+    }
+
+
+
+
+    // cout << "No Error" << endl;
 
     if (make_sym_FLAG == 1){
       for(int s=0;s<num_scales;s++){
@@ -105,20 +132,20 @@ int main(){
         addWeighted(global_activ, 1, activ_vec[c], weights[c],0,global_activ);
         addWeighted(global_inhib, 1, inhib_vec[c], weights[c],0,global_inhib);
       }
-      calc_scale(global_activ,global_inhib,global_chnge,rate);
+      calc_scale(global_activ,global_inhib,global_chnge,global_rate);
     }else{
       for (int c=0;c<num_scales;c++){
-        calc_scale(activ_vec[c],inhib_vec[c],indv_scale[c],rate);
+        calc_scale(activ_vec[c],inhib_vec[c],indv_scale[c],rate[c]);
         addWeighted(global_chnge, 1, indv_scale[c], weights[c],0,global_chnge);
       }
     }
 
 
-    addWeighted(frame, 1, global_chnge, 1,0,frame);
+    addWeighted(frame(Rect(50,50,500,500)), 1, global_chnge, 1,0,frame(Rect(50,50,500,500)));
     normalize(frame,  frame, -127, 127, NORM_MINMAX);
 
     imshow("Frame",frame);
-
+    add_noise(frame(Rect(50,50,500,500)),10,0.7);
 
     global_chnge = 0;
     global_activ = 0;
@@ -149,108 +176,179 @@ int my_remap(int val,int H, int L,int H2,int L2){
 
 
 void calc_reagnet(Mat src, Mat activ,Mat inhib, int p, int n){
+
   Mat padded_Mat = src.clone();
-  int pad = n/2;
-  copyMakeBorder( src, padded_Mat, pad, pad, pad, pad, BORDER_REPLICATE);
+  int pad = 50;//n/2;
+  // copyMakeBorder( src, padded_Mat, pad, pad, pad, pad, BORDER_REPLICATE);
 
   int w_p = p/2;
   int w_n = n/2;
-  int activ_conc,inhib_conc;
-  // pixVal,
+  int pixVal,activ_conc,inhib_conc;
+
 
   for (int row=pad;row<src.rows+pad;row++){
-
-    schar* ac = activ.ptr<schar>(row+1-pad);
-    schar* nh = inhib.ptr<schar>(row+1-pad);
-
     for(int col=pad;col<src.cols+pad;col++){
 
 
-      activ_conc = mean(padded_Mat(Rect(col-w_p,row-w_p,p,p)) )[0];
-      inhib_conc = mean(padded_Mat(Rect(col-w_n,row-w_n,n,n)) )[0];
+      pixVal = padded_Mat.at<schar>(col,row);
 
-      *ac++ = activ_conc;
-      *nh++ = inhib_conc;
+      activ_conc = mean(padded_Mat(Rect(col-w_p,row-w_p,p,p)) )[0];
+      if (activ_conc >= 0){
+        inhib_conc = mean(padded_Mat(Rect(col-w_n,row-w_n,n,n)) )[0];
+
+        activ.at<schar>(row-pad,col-pad) = activ_conc;
+        inhib.at<schar>(row-pad,col-pad) = inhib_conc;
+      }
+
 
     }
   }
+
+// Mat random_pad(Mat src,int pad){
+//   Mat dst(src.cols+pad,src.rows+pad,CV_8SC1);
+//   randu(dst,Scalar(-127),Scalar(127));
+//   return dst;
+// }
+//
+// Mat unpad(Mat src,int pad){
+//
+// }
+  // Mat padded_Mat = src.clone();
+  // int pad = n/2;
+  //
+  // // copyMakeBorder( src, padded_Mat, pad, pad, pad, pad, BORDER_CONSTANT,Scalar(rand()));
+  //
+  // int w_p = p/2;
+  // int w_n = n/2;
+  // int activ_conc,inhib_conc;
+  // // pixVal,
+  //
+  // for (int row=pad;row<src.rows-pad;row++){
+  //
+  //   schar* ac = activ.ptr<schar>(row+1-pad);
+  //   schar* nh = inhib.ptr<schar>(row+1-pad);
+  //
+  //   for(int col=pad;col<src.cols-pad;col++){
+  //
+  //
+  //     activ_conc = mean(padded_Mat(Rect(col-w_p,row-w_p,p,p)) )[0];
+  //     inhib_conc = mean(padded_Mat(Rect(col-w_n,row-w_n,n,n)) )[0];
+  //
+  //     *ac++ = activ_conc;
+  //     *nh++ = inhib_conc;
+  //
+  //   }
+  // }
 
 
 
 }
 
 void calc_scale(Mat activ,Mat inhib,Mat chnge,int rate){
-
-  for(int r=0;r<chnge.rows;++r){
-    schar* ac = activ.ptr<schar>(r);
-    schar* nh = inhib.ptr<schar>(r);
-    schar* ng = chnge.ptr<schar>(r);
-
-    for (int c=0;c<chnge.cols;++c){
-      if(*ac++ >= *nh++){
-        *ng++ = rate;
+  int ac,nh;
+  for (int row=0;row<activ.rows;row++){
+    for(int col=0;col<activ.cols;col++){
+      ac = activ.at<schar>(row,col);
+      nh = inhib.at<schar>(row,col);
+      if (ac >= nh){
+        chnge.at<schar>(row,col) = rate;
       }else{
-        *ng++ = -rate;
+        chnge.at<schar>(row,col) = -rate;
+      }
+
       }
     }
-  }
+  // for(int r=0;r<chnge.rows;++r){
+  //   schar* ac = activ.ptr<schar>(r);
+  //   schar* nh = inhib.ptr<schar>(r);
+  //   schar* ng = chnge.ptr<schar>(r);
+  //
+  //   for (int c=0;c<chnge.cols;++c){
+  //     if(*ac > - 10){
+  //       if(*ac++ >= *nh++){
+  //         *ng++ = rate;
+  //       }else{
+  //         *ng++ = -rate;
+  //       }
+  //     }
+  //
+  //
+  //
+  //   }
+  // }
 
   }
 
 void psy_reagent(Mat src,Mat activ,Mat inhib,int p,int n,int thr){
 
-  Mat padded_Mat = src.clone();
-  int pad = n/2;
-  copyMakeBorder( src, padded_Mat, pad, pad, pad, pad, BORDER_REPLICATE);
-
-  int w_p = p/2;
-  int w_n = n/2;
-  int activ_conc,inhib_conc;
-  // pixVal,
-
-  for (int row=pad;row<src.rows+pad;row++){
-
-    schar* ac = activ.ptr<schar>(row+1-pad);
-    schar* nh = inhib.ptr<schar>(row+1-pad);
-
-    for(int col=pad;col<src.cols+pad;col++){
-
-
-      activ_conc = mean(padded_Mat(Rect(col-w_p,row-w_p,p,p)) )[0];
-      inhib_conc = mean(padded_Mat(Rect(col-w_n,row-w_n,n,n)) )[0];
-
-      if (activ_conc < thr){
-        activ_conc = 0;
-        inhib_conc = 0;
-      }
-
-
-      *ac++ = activ_conc;
-      *nh++ = inhib_conc;
-
-    }
-  }
+  // Mat padded_Mat = src.clone();
+  // int pad = n/2;
+  // copyMakeBorder( src, padded_Mat, pad, pad, pad, pad, BORDER_REPLICATE);
+  //
+  // int w_p = p/2;
+  // int w_n = n/2;
+  // int activ_conc,inhib_conc;
+  // // pixVal,
+  //
+  // for (int row=pad;row<src.rows+pad;row++){
+  //
+  //   schar* ac = activ.ptr<schar>(row+1-pad);
+  //   schar* nh = inhib.ptr<schar>(row+1-pad);
+  //
+  //   for(int col=pad;col<src.cols+pad;col++){
+  //
+  //
+  //     activ_conc = mean(padded_Mat(Rect(col-w_p,row-w_p,p,p)) )[0];
+  //     inhib_conc = mean(padded_Mat(Rect(col-w_n,row-w_n,n,n)) )[0];
+  //
+  //     if (activ_conc < thr){
+  //       activ_conc = rand()%2;
+  //       inhib_conc = rand()%2;
+  //     }
+  //
+  //
+  //     *ac++ = activ_conc;
+  //     *nh++ = inhib_conc;
+  //
+  //   }
+  // }
 }
 
 void make_sym(Mat scale){
   Mat clone = scale.clone();
-  int nRow = clone.rows;
-  int nCols = clone.cols;
-
-  for(int i=0;i<nRow/2;i++){
-    schar* pix_r = scale.ptr<schar>(i+1);
-    schar* pix_rp = scale.ptr<schar>(nRow-i-1);
-
-
-    for(int j=0;j<nCols/2;j++){
-
-      int avg = 0.25*(*(pix_r+j) + *(pix_r+(nCols-j)) + *(pix_rp+j) + *(pix_rp+(nCols-j))  );
-      *(pix_r+j) = avg;
-      *(pix_r+(nCols-j)) =avg;
-      *(pix_rp+j) = avg;
-      *(pix_rp+(nCols-j)) = avg;
+  for(int i=0;i<clone.rows/2+1;i++){
+    for(int j=0;j<clone.cols/2+1;j++){
+      int a = scale.at<schar>(i,j);
+      int b = scale.at<schar>(scale.rows-i,j);
+      int c = scale.at<schar>(i,scale.cols-j);
+      int d =  scale.at<schar>(scale.rows-i,scale.cols-j);
+      double avg = 0.25*(a+b+c+d);
+      scale.at<schar>(i,j) = avg;
+      scale.at<schar>(scale.rows-i,j) = avg;
+      scale.at<schar>(i,scale.cols-j) = avg;
+      scale.at<schar>(scale.rows-i,scale.cols-j) = avg;
     }
   }
+
+
+  // Mat clone = scale.clone();
+  // int nRow = clone.rows;
+  // int nCols = clone.cols;
+  //
+  // for(int i=0;i<nRow/2+1;i++){
+  //   schar* pix_r = scale.ptr<schar>(i+1);
+  //   schar* pix_rp = scale.ptr<schar>(nRow-i-1);
+  //
+  //
+  //   for(int j=0;j<nCols/2+1;j++){
+  //
+  //     int avg = 0.25*(*(pix_r+j) + *(pix_r+(nCols-j)) + *(pix_rp+j) + *(pix_rp+(nCols-j))  );
+  //     *(pix_r+j) = avg;
+  //     *(pix_r+(nCols-j)) =avg;
+  //     *(pix_rp+j) = avg;
+  //     *(pix_rp+(nCols-j)) = avg;
+  //   }
+  // }
 
 }
 
@@ -265,9 +363,9 @@ double my_rand(){
 
 void add_noise(Mat orig,int amount,double prob){
   if (my_rand() >= prob){
-    Mat noise;
+    Mat noise = orig.clone();
     randu(noise,Scalar(-amount),Scalar(amount));
-    orig = orig + noise;
+    addWeighted(orig, 1, noise, 1,0,orig);
   }
 
 }
